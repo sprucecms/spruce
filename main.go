3f570aeb25f58152f41479357aab8b153de6c61b
Aaron Longwell <aaron@culturefoundry.com>
2016-01-16 20:26:43 -0800

    main.go setup and cleanup.

--- main.go
+++ main.go
@@ -13,41 +13,39 @@ import (
 	"github.com/sprucecms/spruce/sprucelib"
 )
 
-const apiPrefix = "/api/v1"
-const adminPrefix = "/admin"
+const apiPrefix = "/api/v1"  // TODO /api/ part should be a config param (and it should be passed dynamically to the JS
+const adminPrefix = "/admin" // TODO /admin/ part should be a config param
 
 func main() {
 	app := sprucelib.NewSpruceApp()
 
 	apiHandler := api.MountAt(apiPrefix, app)
 	cmsHandler := cms.MountAt("/", app)
-
 	adminHandler := http.StripPrefix(adminPrefix, http.FileServer(http.Dir(app.AdminDir())))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("URL: '%s'\n", r.URL.Path)
 		if strings.HasPrefix(r.URL.Path, apiPrefix) {
-			fmt.Printf("apiHandler")
 			apiHandler.ServeHTTP(w, r)
 		} else if strings.HasPrefix(r.URL.Path, adminPrefix) {
-			fmt.Printf("adminHandler")
 			adminHandler.ServeHTTP(w, r)
 		} else {
-			fmt.Printf("cmsHandler")
 			cmsHandler.ServeHTTP(w, r)
 		}
 	})
 
 	// An error here indicates the server failed to start.
 	// Exit ungracefully.
-	httpErr := http.ListenAndServe(":1818", nil)
-	if httpErr != nil {
-		panic(httpErr)
-		os.Exit(1)
-	}
+	go (func() {
+		httpErr := http.ListenAndServe(":1818", nil) // TODO Port should be config param
+		if httpErr != nil {
+			panic(httpErr)
+			os.Exit(1)
+		}
+	})()
 
 	// Wait for SIGINT or SIGTERM to cancel the process
 	ch := make(chan os.Signal)
-	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-	<-ch
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+	r := <-ch
+	fmt.Printf("Spruce shut down: %s", r)
 }
