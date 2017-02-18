769751a6b297e497b05b799ec74b8b873bfbf61b
Aaron Longwell <aaron@culturefoundry.com>
2017-02-18 14:11:57 -0800

    Autotest update.

--- autotest.sh
+++ autotest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # go get github.com/githubnemo/CompileDaemon
-CompileDaemon -build="go build" -command="go test"
+CompileDaemon -build="go build" -command="go test" -recursive -exclude-dir web
