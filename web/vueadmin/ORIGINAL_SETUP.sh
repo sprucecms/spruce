530e20b304bb2c2c57d4db8cb62d5d4a4c9ee751
Aaron Longwell <aaron@culturefoundry.com>
2017-02-18 14:06:39 -0800

    Initial import.

---
+++ web/vueadmin/ORIGINAL_SETUP.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Here for future reference only... this was the series of commands
+# used to initialize this project when it started.
+
+npm init -y
+npm install --save-dev vue
+npm install --save-dev vue-router
+npm install --save-dev vue-resource
+npm install --save-dev vue-loader
+npm install --save-dev webpack
+npm install --save-dev webpack-dev-server
+npm install --save-dev grunt
+npm install --save-dev grunt-contrib-sass
+npm install --save-dev grunt-contrib-jshint
+npm install --save-dev grunt-contrib-watch
